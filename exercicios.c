3fea80d3a8cf8d57575fafa9c9f258b0534f0560
Kalid <kalidcairo@gmail.com>
2026-09-08 09:30:40 -0300

    Update exercicios.c

--- exercicios.c
+++ exercicios.c
@@ -26,3 +26,63 @@ int main(){
 	
 	return 0;
 }
+
+
+
+
+
+
+
+#include <stdio.h>
+
+
+int main(){
+	
+	int opcao;
+	float cel, fah;
+	
+	
+	printf("Se quer em celsius digite 1\n");
+	printf("Se quer em Fahrenheit digite 2\n");
+	scanf("%d", &opcao);
+	
+	
+	switch(opcao){
+		case 1:
+			printf("Entre com o valor de Fahrenheit\n");
+			scanf("%f", &fah);
+			cel = (fah - 32) * 5/9;
+			printf("O valor em celsius eh de %.2f\n", cel);
+			break;
+			
+		case 2:
+			printf("Entre com o valor de Celsius\n");
+			scanf("%f", &cel);
+			fah = (cel * 9/5) + 32;
+			printf("O valor em Fahrenheit eh de %.2f\n", fah);
+			break;
+	}
+	
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
